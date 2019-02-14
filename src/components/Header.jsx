8bbfb3be07d9e9d327f09a1ea64115402341a888
James Cho and Stuart Gill <james@>
2019-02-14 15:15:20 -0800

    add styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,9 @@ function Header() {
     <div>
       <style jsx>
         {`
+        div {
+          margin-bottom: 20px;
+        }
           h1 {
             text-align: center;
             text-shadow: 2px 2px #cccccc;
@@ -15,12 +18,24 @@ function Header() {
             max-width: 100%;
             height: auto;
           }
+
+          button {
+            color: white;
+            background-color: darkgrey;
+            font-size: 1rem;
+            font-weight: bold;
+            width: 150px;
+            height: 50px;
+            margin:auto;
+            border-radius:10%;
+            border: 2px solid black;
+          }
         `}
       </style>
       {/* <img src={farmersImage} /> */}
       <h1>Historical Figures</h1>
-      <Link to="/">Home</Link> |{" "}
-      <Link to="/historicalfigures">Historical Figures</Link>
+      <Link to="/"><button>Home</button></Link> |{" "}
+      <Link to="/historicalfigures"><button>Figures</button></Link>
     </div>
   );
 }
